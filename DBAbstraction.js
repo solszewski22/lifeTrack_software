e598de725adad37137d71e37051619741146403c
solszewski22 <130681019+solszewski22@users.noreply.github.com>
2023-05-05 20:31:39 -0500

    Add files via upload

---
+++ DBAbstraction.js
@@ -0,0 +1,589 @@
+const sqlite3 = require('sqlite3');
+
+class DBAbstraction {
+    constructor(fileName) {
+        this.fileName = fileName;
+    }
+
+    init() {
+        return new Promise((resolve, reject) => {
+            this.db = new sqlite3.Database(this.fileName, async (err) => {
+                if(err) {
+                    reject(err);
+                } else {
+                    try {
+                        await this.createTables();
+                        resolve();
+                    } catch(err) {
+                        reject(err);
+                    }
+                }
+            });
+        });
+    }
+
+    createTables() {
+        const sqlUsers = `
+            CREATE TABLE IF NOT EXISTS "Users" (
+                "id"	INTEGER,
+                "firstName"	TEXT,
+                "lastName"	TEXT,
+                "email"	TEXT,
+                "password"	TEXT,
+                PRIMARY KEY("id")
+            );`;
+        const sqlGoals = `
+            CREATE TABLE IF NOT EXISTS "Goals" (
+                "id"	INTEGER,
+                "title"	TEXT,
+                "description"	INTEGER,
+                "category_id"	INTEGER,
+                PRIMARY KEY("id"),
+                FOREIGN KEY("category_id") REFERENCES "Category"("id")
+            );`;
+        const sqlContacts = `            
+            CREATE TABLE IF NOT EXISTS "Contacts" (
+                "id"	INTEGER,
+                "firstName"	TEXT,
+                "lastName"	TEXT,
+                "phoneNum"	TEXT,
+                "email"	TEXT,
+                PRIMARY KEY("id")
+            );`;
+        const sqlSteps = `            
+            CREATE TABLE IF NOT EXISTS "Steps" (
+                "id"	INTEGER,
+                "title"	TEXT,
+                "stepNum"	INTEGER,
+                "goal_id"	INTEGER,
+                PRIMARY KEY("id"),
+                FOREIGN KEY("goal_id") REFERENCES "Goals"("id")
+            );`;
+        const sqlFiles = `            
+            CREATE TABLE IF NOT EXISTS "Files" (
+                "id"	INTEGER,
+                "title"	TEXT,
+                "file"	BLOB,
+                PRIMARY KEY("id")
+            );`;
+        const sqlNotes = `            
+            CREATE TABLE IF NOT EXISTS "Notes" (
+                "id"	INTEGER,
+                "description"	TEXT,
+                "step_id"	INTEGER,
+                PRIMARY KEY("id"),
+                FOREIGN KEY("step_id") REFERENCES "Steps"("id")
+            );`;
+        const sqlDetails = `            
+            CREATE TABLE IF NOT EXISTS"Details" (
+                "id"	INTEGER,
+                "title"	TEXT,
+                "description"	TEXT,
+                "goal_id"	INTEGER,
+                FOREIGN KEY("goal_id") REFERENCES "Goals"("id"),
+                PRIMARY KEY("id")
+            );`;
+        const sqlAttributes = `
+            CREATE TABLE IF NOT EXISTS "Attributes" (
+                "id"    INTEGER,
+                "title" TEXT,
+                "description"   TEXT,
+                "step_id"   INTEGER,
+                FOREIGN KEY("step_id") REFERENCES "Steps"("id"),
+                PRIMARY KEY("id")
+            );`;
+        const sqlCategories = `
+            CREATE TABLE IF NOT EXISTS "Categories" (
+                "id"    INTEGER,
+                "name"  TEXT,
+                PRIMARY KEY("id")
+            );`;
+        const sqlSharedGoals = `
+            CREATE TABLE IF NOT EXISTS "SharedGoals" (
+                "user_id"   INTEGER,
+                "goal_id"   INTEGER,
+                FOREIGN KEY("user_id") REFERENCES "Users"("id"),
+                FOREIGN KEY("goal_id") REFERENCES "Goals"("id"),
+                PRIMARY KEY("goal_id","user_id")
+            );`;
+        const sqlGoalContact = `            
+            CREATE TABLE IF NOT EXISTS "GoalContact" (
+                "goal_id"	INTEGER,
+                "contact_id"	INTEGER,
+                FOREIGN KEY("contact_id") REFERENCES "Contacts"("id"),
+                FOREIGN KEY("goal_id") REFERENCES "Goals"("id"),
+                PRIMARY KEY("goal_id","contact_id")
+            );`;
+        const sqlStepFile = `            
+            CREATE TABLE IF NOT EXISTS "StepFile" (
+                "step_id"	INTEGER,
+                "file_id"	INTEGER,
+                PRIMARY KEY("step_id","file_id"),
+                FOREIGN KEY("step_id") REFERENCES "Steps"("id"),
+                FOREIGN KEY("file_id") REFERENCES "Files"("id")
+            );`;
+            return new Promise((resolve, reject) => { 
+                this.db.serialize(() => {   
+                    try {
+                        this.db.run(sqlUsers, []);
+                        this.db.run(sqlGoals, []);
+                        this.db.run(sqlContacts, []);
+                        this.db.run(sqlSteps, []);
+                        this.db.run(sqlFiles, []);
+                        this.db.run(sqlNotes, []);
+                        this.db.run(sqlDetails, []);
+                        this.db.run(sqlAttributes, []);
+                        this.db.run(sqlCategories, []);
+                        this.db.run(sqlSharedGoals, []);
+                        this.db.run(sqlGoalContact, []);
+                        this.db.run(sqlStepFile, []);
+                        resolve();
+                    }              
+                    catch(err) {
+                        reject(err);
+                    }
+                }); 
+            });
+    }
+
+    insertUser(firstName, lastName, email, password) {
+        const sql = 'INSERT INTO Users (firstName, lastName, email, password) VALUES (?, ?, ?, ?);'
+        return new Promise((resolve, reject) => {
+            this.db.run(sql, [firstName, lastName, email, password], (err) => {
+                if(err) {
+                    reject(err);
+                }
+                else {
+                    resolve();
+                }
+            });
+        });
+    }
+
+    insertGoal (title, description, category_id) {
+        const sql = 'INSERT INTO Goals (title, description, category_id) VALUES (?, ?, ?);'
+        return new Promise ((resolve, reject) => {
+            this.db.run(sql, [title, description, category_id], function (err) {
+                if(err) {
+                    reject(err);
+                }
+                else {
+                    resolve(this.lastID);
+                }
+            });
+        });
+    }
+
+    insertDetail (title, description, goal_id) {
+        const sql = `INSERT INTO Details (title, description, goal_id) VALUES (?, ?, ?);`
+        return new Promise ((resolve, reject) => {
+            this.db.run(sql, [title, description, goal_id], (err) => {
+                if(err) {
+                    reject(err);
+                }
+                else {
+                    resolve();
+                }
+            });
+        });
+    }
+
+    insertContact (firstName, lastName, phoneNum, email) {
+        const sql = `INSERT INTO Contacts (firstName, lastName, phoneNum, email) VALUES (?, ?, ?, ?);`;
+        return new Promise ((resolve, reject) => {
+            this.db.run(sql, [firstName, lastName, phoneNum, email], function (err) {
+                if(err) {
+                    reject(err);
+                }
+                else {
+                    resolve(this.lastID);
+                }
+            });
+        });
+    }
+
+    insertGoalContact(goalID, contactID) {
+        const sql = `INSERT INTO GoalContact (goal_id, contact_id) VALUES (?, ?);`;
+        return new Promise ((resolve, reject) => {
+            this.db.run(sql, [goalID, contactID], (err) => {
+                if(err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            });
+        });
+    }
+
+    insertDescription (description, goalID) {
+        const sql = `
+            UPDATE Goals
+            SET description = ?
+            WHERE id = ?
+        ;`;
+        return new Promise ((resolve, reject) => {
+            this.db.run(sql, [description, goalID], (err) => {
+                if(err) {
+                    reject(err);
+                }
+                else {
+                    resolve();
+                }
+            });
+        });
+    }
+
+    insertStep (title, stepNum, status, goal_id) {
+        const sql = `INSERT INTO Steps (title, stepNum, status, goal_id) VALUES (?, ?, ?, ?);`;
+        return new Promise ((resolve, reject) => {
+            this.db.run(sql, [title, stepNum, status, goal_id], (err) => {
+                if(err) {
+                    reject(err);
+                }
+                else {
+                    resolve(err);
+                }
+            });
+        });
+    }
+
+    insertAttribute (title, description, step_id) {
+        const sql = `INSERT INTO Attributes (title, description, step_id) VALUES (?, ?, ?);`;
+        return new Promise ((resolve, reject) => {
+            this.db.run(sql, [title, description, step_id], (err) => {
+                if(err) {
+                    reject(err);
+                }
+                else {
+                    resolve();
+                }
+            });
+        });
+    }
+
+    insertNote (description, step_id) {
+        const sql = `INSERT INTO Notes (description, step_id) VALUES (?, ?);`;
+        return new Promise ((resolve, reject) => {
+            this.db.run(sql, [description, step_id], (err) => {
+                if(err) {
+                    reject(err);
+                }
+                else {
+                    resolve();
+                }
+            });
+        });
+    }
+
+    insertCollaborator(user_id, goal_id) {
+        const sql = `INSERT INTO SharedGoals (user_id, goal_id) VALUES (?, ?);`;
+        return new Promise ((resolve, reject) => {
+            this.db.run(sql, [user_id, goal_id], (err) => {
+                if(err) {
+                    reject(err);
+                }
+                else {
+                    resolve();
+                }
+            });
+        });
+    }
+
+    insertCategory(categoryName) {
+        const sql = `INSERT INTO Categories (name) VALUES (?);`;
+        return new Promise ((resolve, reject) => {
+            this.db.run(sql, [categoryName], (err) => {
+                if(err) {
+                    reject(err);
+                }
+                else {
+                    resolve();
+                }
+            });
+        });
+    }
+
+    getCategoryID (categoryName) {
+        const sql = `
+            SELECT id
+            FROM Categories
+            WHERE name = ? COLLATE NOCASE;
+        `;
+        return new Promise ((resolve, reject) => {
+            this.db.get(sql, [categoryName], (err, row) => {
+                if(err)
+                {
+                    reject(err);
+                } else {
+                    resolve(row);
+                }
+            });
+        });
+    }
+
+    getGoalID (goalTitle)
+    {
+        const sql = `
+            SELECT id
+            FROM Goals
+            WHERE title = ? COLLATE NOCASE;
+        `;
+        return new Promise ((resolve, reject) => {
+            this.db.get(sql, [goalTitle], (err, row) => {
+                if(err)
+                {
+                    reject(err);
+                } else {
+                    resolve(row);
+                }
+            });
+        });
+    }
+
+    getStepID (stepTitle) {
+        const sql = `
+            SELECT id
+            FROM Steps
+            WHERE title = ? COLLATE NOCASE
+        ;`;
+        return new Promise ((resolve, reject) => {
+            this.db.get(sql, [stepTitle], (err, row) => {
+                if(err) {
+                    reject(err);
+                }
+                else {
+                    resolve(row);
+                }
+            });
+        });
+    }
+
+    getUser(firstName) {
+        firstName = firstName + "%";
+        const sql = `
+            SELECT firstName, lastName, email
+            FROM Users
+            WHERE firstName LIKE ?
+        ;`;
+        return new Promise ((resolve, reject) => {
+            this.db.all(sql, [firstName], (err, row) => {
+                if(err) {
+                    reject(err);
+                }
+                else {
+                    resolve(row);
+                }
+            })
+        });
+    }
+
+    getUserID(email) {
+        const sql = `
+            SELECT id
+            FROM Users
+            WHERE email = ?
+        ;`;
+        return new Promise ((resolve, reject) => {
+            this.db.get(sql, [email], (err, row) => {
+                if(err) {
+                    reject(err);
+                }
+                else {
+                    resolve(row);
+                }
+            });
+        });
+    }
+
+    getAllGoals(userEmail) {
+        const sql = `
+            SELECT Goals.title
+            FROM Users, Goals, SharedGoals
+            WHERE SharedGoals.user_id = Users.id
+            AND SharedGoals.goal_id = Goals.id
+            AND Users.email = ?
+        ;`;
+        return new Promise ((resolve, reject) => {
+            this.db.all(sql, [userEmail], (err, row) => {
+                if(err) {
+                    reject(err);
+                }
+                else {
+                    resolve(row);
+                }
+            });
+        });
+    }
+
+    getUserGoals (userID) {
+        const sql =`
+            SELECT Goals.title
+            FROM Users, Goals, SharedGoals
+            WHERE SharedGoals.user_id = Users.id
+            AND SharedGoals.goal_id = Goals.id
+            GROUP BY goal_id
+            HAVING COUNT(*) = 1
+            AND user_id = ?
+        ;`;
+        return new Promise ((resolve, reject) => {
+            this.db.all(sql, [userID], (err,row) => {
+                if(err) {
+                    reject(err);
+                }
+                else {
+                    resolve(row);
+                }
+            });
+        });
+    }
+
+    getSharedGoals (userID) {
+        const sql = `
+            SELECT Goals.title
+            FROM Users, Goals, SharedGoals
+            WHERE SharedGoals.user_id = Users.id
+            AND SharedGoals.goal_id = Goals.id
+            AND Goals.id IN (
+                SELECT goal_id
+                FROM Users, Goals, SharedGoals
+                WHERE SharedGoals.user_id = Users.id
+                AND SharedGoals.goal_id = Goals.id
+                GROUP BY goal_id
+                HAVING COUNT(*) > 1
+            )
+            AND Users.id = ?
+        ;`;
+        return new Promise ((resolve, reject) => {
+            this.db.all(sql, [userID], (err, rows) => {
+                if(err) {
+                    reject(err);
+                }
+                else {
+                    resolve(rows);
+                }
+            });
+        });
+    }
+
+    getStepsAndStatus (goalTitle) {
+        const sql = `
+            SELECT status
+            FROM Steps, Goals
+            WHERE Steps.goal_id = Goals.id
+            AND Goals.title = ?
+        ;`;
+        return new Promise ((resolve, reject) => {
+            this.db.all(sql, [goalTitle], (err, row) => {
+                if(err) {
+                    reject(err);
+                }
+                else {
+                    resolve(row);
+                }
+            });
+        });
+    }
+
+    getByCategory (email, category) {
+        const sql = `
+            SELECT Goals.title
+            FROM Categories, Goals, SharedGoals, Users
+            WHERE Goals.category_id = Categories.id
+            AND SharedGoals.user_id = Users.id
+            AND SharedGoals.goal_id = Goals.id
+            AND Users.email = ?
+            AND Categories.name = ?
+        ;`;
+        return new Promise ((resolve, reject) => {
+            this.db.all(sql, [email, category], (err, row) => {
+                if(err) {
+                    reject(err);
+                }
+                else {
+                    resolve(row);
+                }
+            });
+        });
+    }
+
+    getBySearchGoal (email, title) {
+        title = title + "%";
+        const sql = `
+            SELECT Goals.title
+            FROM SharedGoals, Goals, Users
+            WHERE SharedGoals.goal_id = Goals.id
+            AND SharedGoals.user_id = Users.id
+            AND Users.email = ?
+            AND Goals.title LIKE ?
+        ;`;
+        return new Promise ((resolve, reject) => {
+            this.db.all(sql, [email, title], (err, row) => {
+                if(err) {
+                    reject(err);
+                }
+                else {
+                    resolve(row);
+                }
+            });
+        });
+    }
+
+    getContact (title) {
+        const sql = `
+            SELECT Contacts.firstName, Contacts.lastName, Contacts.phoneNum, Contacts.email
+            FROM Goals, Contacts, GoalContact
+            WHERE GoalContact.goal_id = Goals.id
+            AND GoalContact.contact_id = Contacts.id
+            AND Goals.title = ?
+        ;`;
+        return new Promise ((resolve, reject) => {
+            this.db.all(sql, [title], (err, row) => {
+                if(err) {
+                    reject(err);
+                }
+                else {
+                    resolve(row);
+                }
+            });
+        });
+    }
+
+    getCredentials (email, password) {
+        const sql = `
+            SELECT firstName
+            FROM Users
+            WHERE Users.email = ?
+            AND Users.password = ?
+        ;`;
+        return new Promise ((resolve, reject) => {
+            this.db.get(sql, [email, password], (err, row) => {
+                if(err) {
+                    reject(err);
+                }
+                else {
+                    resolve(row);
+                }
+            });
+        });
+    }
+
+    resetPassword(oldPassword, newPassword) {
+        const sql = `
+            UPDATE Users 
+            SET password = ?
+            WHERE password = ?
+        ;`;
+        return new Promise ((resolve, reject) => {
+            this.db.run(sql, [newPassword, oldPassword], (err) => {
+                if(err) {
+                    reject(err);
+                }
+                else {
+                    resolve();
+                }
+            });
+        });
+    }
+}
+
+module.exports = DBAbstraction;
---
+++ index.js
@@ -0,0 +1,316 @@
+'use strict'
+
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require("body-parser");
+const DBAbstraction = require('./DBAbstraction');
+
+const db = new DBAbstraction('./data/software.sqlite');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static('life_track/build'));
+
+app.post('/newUser', async (req, res) => {
+    try {
+        const firstName = req.body.firstName;
+        const lastName = req.body.lastName;
+        const email = req.body.email;
+        const password = req.body.password;
+
+        await db.insertUser(firstName, lastName, email, password);
+
+        res.json({"result" : "success"});
+    }
+    catch (err) {
+        res.jsob(err);
+    }
+})
+
+app.post('/newGoal', async (req, res) => {
+    try {
+        const categoryID = await db.getCategoryID(req.body.categoryName);
+        const userID = await db.getUserID(req.body.email);
+
+        if(categoryID)
+        {
+            const title = req.body.title;
+            const description = req.body.description;
+            const category_id = categoryID.id;
+
+            const goalID = await db.insertGoal(title, description, category_id);
+
+            await db.insertCollaborator(userID.id, goalID);
+
+            res.json({"result" : "success"});
+        }
+        else {
+            res.json({"categoty results" : "None"});
+        }   
+    }
+    catch (err) {
+        res.json(err);
+    }
+});
+
+app.post('/newDetail', async (req, res) => {
+    try {
+        const goalID = await db.getGoalID(req.body.goalTitle);
+        if(goalID)
+        {
+            const title = req.body.title;
+            const description = req.body.description;
+            const goal_id = goalID.id;
+
+            await db.insertDetail(title, description, goal_id);
+            res.json({"result" : "success"});
+        } else {
+            res.json({"goal results" : "none"});
+        }
+    }
+    catch (err) {
+        res.json(err);
+    }
+});
+
+app.post('/newContact', async (req, res) => {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const phoneNum = req.body.phoneNum;
+    const email = req.body.email;
+
+    try {
+        const contactID = await db.insertContact(firstName, lastName, phoneNum, email);
+        const goalID = await db.getGoalID(req.body.goalTitle);
+
+        await db.insertGoalContact(goalID.id, contactID);
+        res.json({"results" : "success"});
+    }
+    catch (err) {
+        res.json(err);
+    }
+});
+
+app.post('/updateDescription', async (req, res) => {
+    try {
+        const goalID = await db.getGoalID(req.body.goalTitle);
+        const description = req.body.description;
+
+        await db.insertDescription(description, goalID.id);
+
+        res.json({"results" : "success"});
+    }
+    catch(err) {
+        res.json(err);
+    }
+});
+
+app.post ('/newStep', async (req, res) => {
+    try {
+        const goalID = await db.getGoalID(req.body.goalTitle);
+
+        const stepTitle = req.body.title;
+        const stepNum = req.body.stepNum;
+        const status = req.body.status;
+
+        await db.insertStep(stepTitle, stepNum, status, goalID.id);
+
+        res.json({"results" : "success"});
+    }
+    catch (err) {
+        res.json(err);
+    }
+    
+});
+
+app.post ('/newAttribute', async (req, res) => {
+    try {
+        const stepID = await db.getStepID(req.body.stepTitle);
+
+        const title = req.body.title;
+        const description = req.body.description;
+
+        await db.insertAttribute(title, description, stepID.id);
+        res.json({"results" : "success"});
+    }
+    catch (err) {
+        res.json(err);
+    }
+});
+
+app.post ('/newNote', async (req, res) => {
+    const stepID = await db.getStepID(req.body.stepTitle);
+    try {
+        const description = req.body.description;
+
+        await db.insertNote(description, stepID.id);
+        res.json({"results" : "success"});
+    }
+    catch (err) {
+        res.json(err);
+    }
+});
+
+app.get('/searchByUser', async (req, res) => {
+    const firstName = req.body.firstName;
+    try {
+        const users = await db.getUser(firstName);
+        res.json(users)
+    }
+    catch (err) {
+        res.json(err);
+    }
+});
+
+app.post ('/addCollaborator', async (req, res) => {
+    try {
+        const userID = await db.getUserID(req.body.email);
+        const goalID = await db.getGoalID(req.body.goalTitle);
+
+        await db.insertCollaborator(userID.id, goalID.id);
+        res.json({"results" : "success"});
+    }
+    catch(err) {
+        res.json(err);
+    }
+});
+
+app.post ('/newCategory', async(req, res) => {
+    const categoryName = req.body.categoryName;
+    try {
+        await db.insertCategory(categoryName);
+        res.json({"results" : "success"});
+    }
+    catch(err) {
+        res.json(err);
+    }
+});
+
+app.get('/displayAllGoals', async (req, res) => {
+    try {
+        const allGoals = await db.getAllGoals(req.body.email);
+        res.json(allGoals);
+    }
+    catch(err) {
+        res.json(err);
+    }
+});
+
+app.get('/displayUserGoals', async (req, res) => {
+    try {
+        const userID = await db.getUserID(req.body.email);
+        const UserGoals = await db.getUserGoals(userID.id);
+        res.json(UserGoals);
+    }
+    catch(err) {
+        res.json(err);
+    }
+});
+
+app.get('/displaySharedGoals', async (req, res) => {
+    try {
+        const userID = await db.getUserID(req.body.email);
+        const sharedGoals = await db.getSharedGoals(userID.id);
+        res.json(sharedGoals);
+    }
+    catch(err) {
+        res.json(err);
+    }
+});
+
+app.get('/completedGoals', async (req, res) => {
+    try {
+        let completedGoals = [];
+        const userGoals = await db.getAllGoals(req.body.email);
+        for(let i = 0; i < userGoals.length; i++) {
+            const stepsAndStatus = await db.getStepsAndStatus(userGoals[i]["title"]);
+            let retVal = true;
+            for(let j = 0; j < stepsAndStatus.length; j++) {
+                if(stepsAndStatus[j]["status"]  === "incomplete") {
+                    retVal = false;
+                    break;
+                }
+            }
+            if(retVal === true) {
+                completedGoals.push(userGoals[i]);
+            }
+        }
+        res.json(completedGoals);
+    }
+    catch (err) {
+        res.json(err);
+    }
+});
+
+app.get('/filterCategory', async(req, res) => {
+    try {
+        const categoryGoals = await db.getByCategory(req.body.email, req.body.category);
+        res.json(categoryGoals);
+    }
+    catch(err){
+        res.json(err);
+    }
+});
+
+app.get('/search', async(req, res) => {
+    try {
+        const searchResults = await db.getBySearchGoal(req.body.email, req.body.title);
+        res.json(searchResults);
+    }
+    catch(err) {
+        res.json(err);
+    }
+});
+
+app.get('/contact', async (req, res) => {
+    try {
+        const goalContact = await db.getContact(req.body.goalTitle);
+        res.json(goalContact);
+    }
+    catch(err) {
+        res.json(err);
+    }
+});
+
+// get goal and step data
+
+app.get('/validation', async (req, res) => {
+    try {
+        const credientials = await db.getCredentials(req.body.email, req.body.password);
+        if(credientials == undefined) {
+            res.json({"result" : "no user"});
+        } else {
+            res.json(credientials);
+        }
+    }
+    catch(err) {
+        res.json(err);
+    }
+});
+
+app.post('/reset-password', async (req, res) => {
+    try {
+        await db.resetPassword(req.body.oldPassword, req.body.newPassword);
+        res.json({"result" : "success"});
+    }
+    catch(err) {
+        res.json(err);
+    }
+});
+
+app.use((req,res) => {
+    res.status(404).json({badRequest: `${req.url} cannot be found here`});
+});
+
+db.init()
+    .then(() => {
+        app.listen(53140, () => console.log('The server is up and running...'));
+    })
+    .catch(err => {
+        console.log('Problem setting up the database');
+        console.log(err);
+    });
